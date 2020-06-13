7cedeca3708ee5cfd1a23427c5c3648eb6573aa6
Josh Berk <joshbmac1@yahoo.com>
2020-06-13 18:04:23 -0400

    Update main.py
    Finished koch_draw() function
      set initial position
      maintains starting position and heading
      moved koch() algorithym function inside of koch_draw() to make private
    
    Added tree_draw() function to draw tree fractals

--- main.py
+++ main.py
@@ -3,30 +3,36 @@ turtle graphics
 """
 
 import turtle
-import tkinter
-
-def koch(t,i=0,sl=100):
-    if i == 0:
-        t.fd(sl)
-    elif i > 0:
-        koch(t,i=i-1,sl=sl/3)
-        t.rt(60)
-        koch(t,i=i-1,sl=sl/3)
-        t.lt(120)
-        koch(t,i=i-1,sl=sl/3)
-        t.rt(60)
-        koch(t,i=i-1,sl=sl/3)
+import math
 
 def koch_draw(t,i=0,sl=100):
     """
     start drawing a koch snowflake\n
     t - turtle to command\n
-    i - iterations\n
+    i - recursion level\n
     sl - initial side length in pixels
     """
+    # start algorithym #
+    def koch(t,i=0,sl=100):
+        if i == 0:
+            t.fd(sl)
+        elif i > 0:
+            koch(t,i=i-1,sl=sl/3)
+            t.rt(60)
+            koch(t,i=i-1,sl=sl/3)
+            t.lt(120)
+            koch(t,i=i-1,sl=sl/3)
+            t.rt(60)
+            koch(t,i=i-1,sl=sl/3)
+    # end algorithym #
+
     # positioning #
     # put the center of the triangle on the center of the canvas
-
+    # vector to the appropriate start position
+    t.pu()
+    t.rt(150)
+    t.fd( sl / math.sqrt(3))
+    t.lt(150)
     # end positioning #
 
     # draw the snowflake #
@@ -37,8 +43,40 @@ def koch_draw(t,i=0,sl=100):
     t.lt(120)
     koch(t,i=i,sl=sl)
     t.lt(120)
-    t.pu()
     # end draw snowflake #
+
+    # return home
+    t.pu()
+    t.lt(30)
+    t.fd( sl / math.sqrt(3))
+    t.rt(30)
+
+def tree_draw(t,i=0,sl=100,fctr=.8,theta=20):
+    """
+    will draw a tree fractal
+
+    t - turtle to command\n
+    i - recursion depth\n
+    sl - stem length\n
+    fctr - size of deeper stem related to current stem
+    theta - angle of branches
+    """
+    if i > 0:
+        t.pd()
+        t.fd(sl)
+        t.lt(theta)
+        tree_draw(t,i=i-1,sl=sl*fctr,fctr=fctr,theta=theta)
+        t.pu()
+        t.rt(2 * theta)
+        t.pd()
+        tree_draw(t,i=i-1,sl=sl*fctr,fctr=fctr,theta=theta)
+        t.pu()
+        t.lt(theta)
+        t.bk(sl)
+    elif i == 0:
+        t.fd(sl)
+        t.bk(sl)
+
 
 def main():
     """
@@ -65,16 +103,37 @@ def main():
     t = turtle.RawTurtle(s)
 
     # show the turtle
-    # t.showturtle()
-    t.hideturtle()
+    t.showturtle()
+    # t.hideturtle()
 
     # set turtle speed
     t.speed(0)
+
+    # start with the pen up
+    t.pu()
     # end turtle setup #
 
-    # draw snowflake
-    koch_draw(t,i=3,sl=400)
+    # draw here #
+    # sl=150
+    # t.setpos(-200,100)
+    # koch_draw(t, i=0, sl=sl)
+    # t.setpos(0,100)
+    # koch_draw(t, i=1, sl=sl)
+    # t.setpos(200,100)
+    # koch_draw(t, i=2, sl=sl)
+    # t.setpos(-200,-100)
+    # koch_draw(t, i=3, sl=sl)
+    # t.setpos(0,-100)
+    # koch_draw(t, i=4, sl=sl)
+    # t.setpos(200,-100)
+    # koch_draw(t, i=5, sl=sl)
 
+    t.seth(90)
+    t.setpos(0,-100)
+    tree_draw(t,i=15)
+    # end drawing #
+    
 
+    # halt till input
+    input()
 main()
-input()
